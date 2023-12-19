39c7b7d97a63a55937c717fce8874ed9dd37d013
ubuntu <itishrishikesh@gmail.com>
2023-12-19 18:07:29 +0000

    jwt helper functions

---
+++ auth/jwt.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodEdDSA)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(10 * time.Minute)
+	claims["authorized"] = true
+	claims["user"] = "username"
+	tokenString, err := token.SignedString([]byte(os.Getenv("KEY")))
+	if err != nil {
+		fmt.Println("E#1PBTIY - Failed to Generate JWT Token")
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) *jwt.Token {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("KEY")), nil
+	})
+	if err != nil {
+		fmt.Println("E#1PBU9G - Failed to verify token.")
+	}
+	return token
+}
