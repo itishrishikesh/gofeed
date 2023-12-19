ceb0cd89e9d34279fd39f3d75d8977e1ccd65446
ubuntu <itishrishikesh@gmail.com>
2023-12-19 17:16:14 +0000

    sqlcForVersion7Migration

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -8,5 +8,6 @@ SELECT * FROM users WHERE api_key = $1;
 
 -- name: UpdateAPIKey :one
 UPDATE users 
-SET api_key = encode(sha256(random()::text::bytea) 
+SET api_key = encode(sha256(random()::text::bytea), 'hex') 
 WHERE id = $1
+RETURNING *;
